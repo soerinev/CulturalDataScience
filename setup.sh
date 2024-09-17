792e6760cd2e239467fdab9266d04b450784aaf4
“soerinevgit config --global user.name “soerinev <laura.soerine@icloud.com>
2024-09-17 12:16:04 +0200

    maybe adding class exercise document??

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # how to setup automatically when I open Ucloud
+!/usr/bin/env
+
 git config --global user.email "laura.soerine@icloud.com"
 git config --global user.name "soerinev"
 
-# before adding, committing and pushing to github, press "bash setup.sh", that's the same as writing the above
+# before adding, committing and pushing to github, press "bash setup.sh", that's the same as writing the above?? not necessary because of !/usr/bin/env?? 
